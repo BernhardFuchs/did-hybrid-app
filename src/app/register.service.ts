f2780f89b788c986b276a506c9554681b4f4ff5a
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2019-11-11 05:32:46 +0100

    Barcode Scanner implementation.

---
+++ src/app/register.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  constructor() {}
+
+  public register(url: string) {
+    console.log('###Register for url: ', url);
+  }
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { RegisterService } from '../register.service';
+import { ScannerService } from './scanner.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnDestroy {
+  private scanSubscription: Subscription;
 
-  constructor() {}
+  constructor(
+    private registerService: RegisterService,
+    private scannerService: ScannerService
+  ) {}
 
+  public scanBarcodePlugin() {
+    this.scanSubscription = this.scannerService
+      .getText()
+      .subscribe((text: string) => this.registerService.register(text));
+  }
+
+  ngOnDestroy(): void {
+    this.scanSubscription.unsubscribe();
+  }
 }
