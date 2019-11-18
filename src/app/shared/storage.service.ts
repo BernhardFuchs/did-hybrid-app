645ef1b977110bf21054c49255ef93eb0e83de96
Bernhard Fuchs <bernhard.fuchs@paysafe.com>
2019-11-18 10:51:19 +0100

    Resolve Did Document for identifier.

--- src/app/shared/storage.service.ts
+++ src/app/shared/storage.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import 'capacitor-secure-storage-plugin';
 import { Plugins } from '@capacitor/core';
-import { Observable } from 'rxjs';
+import { defer, Observable } from 'rxjs';
 import { fromPromise } from 'rxjs/internal-compatibility';
 const { SecureStoragePlugin } = Plugins;
 
@@ -17,6 +17,17 @@ export class StorageService {
       key: this.IDENTIFIER_KEY,
       value: identifier
     };
-    return fromPromise(SecureStoragePlugin.set(options));
+    return fromPromise(
+      SecureStoragePlugin.set(options).then(result => result.value)
+    );
+  }
+
+  public getIdentifier(): Observable<string> {
+    const options = {
+      key: this.IDENTIFIER_KEY
+    };
+    return fromPromise(
+      SecureStoragePlugin.get(options).then(result => result.value)
+    );
   }
 }
